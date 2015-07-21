e93dfb5a7ca066761e1d5b5ce2ff2cfa2172812c
pushnoy100500 <brainstation@BrainStations-MacBook-Air-6.local>
2015-07-21 11:16:13 -0400

    added user model, run migration for it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720181804) do
+ActiveRecord::Schema.define(version: 20150721150630) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20150720181804) do
     t.boolean  "is_published"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "email"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
